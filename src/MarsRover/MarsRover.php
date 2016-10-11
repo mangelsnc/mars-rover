0c1884c6a1668323036342c3dead63482b17d123
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2016-10-11 09:03:30 +0200

    First iteration

--- src/MarsRover/MarsRover.php
+++ src/MarsRover/MarsRover.php
@@ -7,5 +7,143 @@ namespace MarsRover;
  */
 class MarsRover
 {
+    const NORTH = 'n';
+    const EAST = 'e';
+    const SOUTH = 's';
+    const WEST = 'w';
 
+    const FORWARD = 'f';
+    const BACKWARD = 'b';
+    const RIGHT = 'r';
+    const LEFT = 'l';
+
+    private $direction;
+    private $x;
+    private $y;
+
+
+
+    public function __construct($direction = self::NORTH, $position = [0, 0])
+    {
+        $this->direction = $direction;
+        $this->x = $position[0];
+        $this->y = $position[1];
+    }
+
+    public function getPosition()
+    {
+        return [$this->x, $this->y];
+    }
+
+    public function getDirection()
+    {
+        return $this->direction;
+    }
+
+    public function move($movement)
+    {
+        if (self::NORTH === $this->getDirection()) {
+            if (self::FORWARD === $movement) {
+                $this->y++;
+
+                return true;
+            }
+
+            if (self::BACKWARD === $movement) {
+                $this->y--;
+
+                return true;
+            }
+
+            if (self::RIGHT === $movement) {
+                $this->direction = self::EAST;
+
+                return true;
+            }
+
+            if (self::LEFT === $movement) {
+                $this->direction = self::WEST;
+
+                return true;
+            }
+        }
+
+        if (self::EAST === $this->getDirection()) {
+            if (self::FORWARD === $movement) {
+                $this->x++;
+
+                return true;
+            }
+
+            if (self::BACKWARD === $movement) {
+                $this->x--;
+
+                return true;
+            }
+
+            if (self::RIGHT === $movement) {
+                $this->direction = self::SOUTH;
+
+                return true;
+            }
+
+            if (self::LEFT === $movement) {
+                $this->direction = self::NORTH;
+
+                return true;
+            }
+        }
+
+        if (self::SOUTH === $this->getDirection()) {
+            if (self::FORWARD === $movement) {
+                $this->y--;
+
+                return true;
+            }
+
+            if (self::BACKWARD === $movement) {
+                $this->y++;
+
+                return true;
+            }
+
+            if (self::RIGHT === $movement) {
+                $this->direction = self::WEST;
+
+                return true;
+            }
+
+            if (self::LEFT === $movement) {
+                $this->direction = self::EAST;
+
+                return true;
+            }
+        }
+
+        if (self::WEST === $this->getDirection()) {
+            if (self::FORWARD === $movement) {
+                $this->x--;
+
+                return true;
+            }
+
+            if (self::BACKWARD === $movement) {
+                $this->x++;
+
+                return true;
+            }
+
+            if (self::RIGHT === $movement) {
+                $this->direction = self::NORTH;
+
+                return true;
+            }
+
+            if (self::LEFT === $movement) {
+                $this->direction = self::SOUTH;
+
+                return true;
+            }
+        }
+    }
 }
--- src/MarsRover/Tests/MarsRoverTest.php
+++ src/MarsRover/Tests/MarsRoverTest.php
@@ -5,5 +5,215 @@ use MarsRover\MarsRover;
 
 class MarsRoverTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     */
+    public function itShouldStartByDefaultAtZeroPoint()
+    {
+        $marsRover = new MarsRover();
 
+        $this->assertEquals([0, 0], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldStartByDefaultAtDirectionNorth()
+    {
+        $marsRover = new MarsRover();
+
+        $this->assertEquals(MarsRover::NORTH, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesForwardWhenAskedForIt()
+    {
+        $marsRover = new MarsRover();
+
+        $marsRover->move(MarsRover::FORWARD);
+
+        $this->assertEquals([0, 1], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesBackwardWhenAskedForIt()
+    {
+        $marsRover = new MarsRover();
+
+        $marsRover->move(MarsRover::BACKWARD);
+
+        $this->assertEquals([0, -1], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesToRightWhenDirectionIsEastAndMoveIsForward()
+    {
+        $marsRover = new MarsRover(MarsRover::EAST, [0, 0]);
+
+        $marsRover->move(MarsRover::FORWARD);
+
+        $this->assertEquals([1, 0], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesToLeftWhenDirectionIsWestAndMoveIsBackward()
+    {
+        $marsRover = new MarsRover(MarsRover::EAST, [0, 0]);
+
+        $marsRover->move(MarsRover::BACKWARD);
+
+        $this->assertEquals([-1, 0], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesToDownWhenDirectionIsSouthAndMoveIsForward()
+    {
+        $marsRover = new MarsRover(MarsRover::SOUTH, [0, 0]);
+
+        $marsRover->move(MarsRover::FORWARD);
+
+        $this->assertEquals([0, -1], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesToUpWhenDirectionIsSouthAndMoveIsBackward()
+    {
+        $marsRover = new MarsRover(MarsRover::SOUTH, [0, 0]);
+
+        $marsRover->move(MarsRover::BACKWARD);
+
+        $this->assertEquals([0, 1], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesToLeftWhenDirectionIsWestAndMoveIsForward()
+    {
+        $marsRover = new MarsRover(MarsRover::WEST, [0, 0]);
+
+        $marsRover->move(MarsRover::FORWARD);
+
+        $this->assertEquals([-1, 0], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldMovesToRightWhenDirectionIsWestAndMoveIsBackward()
+    {
+        $marsRover = new MarsRover(MarsRover::WEST, [0, 0]);
+
+        $marsRover->move(MarsRover::BACKWARD);
+
+        $this->assertEquals([1, 0], $marsRover->getPosition());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToEastWhenDirectionIsNorthAndAskedToMoveRight()
+    {
+        $marsRover = new MarsRover();
+
+        $marsRover->move(MarsRover::RIGHT);
+
+        $this->assertEquals(MarsRover::EAST, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToWestWhenDirectionIsNorthAndAskedToMoveLeft()
+    {
+        $marsRover = new MarsRover();
+
+        $marsRover->move(MarsRover::LEFT);
+
+        $this->assertEquals(MarsRover::WEST, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToSouthWhenDirectionIsEastAndAskedToMoveRight()
+    {
+        $marsRover = new MarsRover(MarsRover::EAST);
+
+        $marsRover->move(MarsRover::RIGHT);
+
+        $this->assertEquals(MarsRover::SOUTH, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToNorthWhenDirectionIsEastAndAskedToMoveLeft()
+    {
+        $marsRover = new MarsRover(MarsRover::EAST);
+
+        $marsRover->move(MarsRover::LEFT);
+
+        $this->assertEquals(MarsRover::NORTH, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToWestWhenDirectionIsSouthAndAskedToMoveRight()
+    {
+        $marsRover = new MarsRover(MarsRover::SOUTH);
+
+        $marsRover->move(MarsRover::RIGHT);
+
+        $this->assertEquals(MarsRover::WEST, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToEastWhenDirectionIsSouthAndAskedToMoveLeft()
+    {
+        $marsRover = new MarsRover(MarsRover::SOUTH);
+
+        $marsRover->move(MarsRover::LEFT);
+
+        $this->assertEquals(MarsRover::EAST, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToNorthWhenDirectionIsWestAndAskedToMoveRight()
+    {
+        $marsRover = new MarsRover(MarsRover::WEST);
+
+        $marsRover->move(MarsRover::RIGHT);
+
+        $this->assertEquals(MarsRover::NORTH, $marsRover->getDirection());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRotateToSouthWhenDirectionIsWestAndAskedToMoveLeft()
+    {
+        $marsRover = new MarsRover(MarsRover::WEST);
+
+        $marsRover->move(MarsRover::LEFT);
+
+        $this->assertEquals(MarsRover::SOUTH, $marsRover->getDirection());
+    }
 }
